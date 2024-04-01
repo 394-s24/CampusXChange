78115b6f2ff85d47ee50600e2b70b30ea044959d
Rachel Yao <42477010+rachelya0@users.noreply.github.com>
2024-04-01 00:34:35 -0500

    made search non case-sensitive, added filtering button functionality, formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faSearch, faEnvelope, faStore, faFilter, faLessThan } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faSearch, faEnvelope, faStore, faFilter, faUser } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 import data from './fakedata.json';
 import PriceFilter from './components/PriceFilter';
@@ -13,6 +13,7 @@ const App = () => {
   const [activePriceFilter, setActivePriceFilter] = useState(Infinity);
   const [activeTags, setActiveTags] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const [showFilters, setShowFilters] = useState(false);
 
   const filterprices = [10, 25, 50, 75, 100];
 
@@ -30,10 +31,9 @@ const App = () => {
 
       filtered = temp;
     }
-
-
     if (searchValue.length > 0) {
-      filtered = filtered.filter(item => item.name.includes(searchValue))
+      // non case-sensitive
+      filtered = filtered.filter(item => item.name.toLowerCase().includes(searchValue.toLowerCase()))
     }
 
     setFilteredItems(filtered);
@@ -42,25 +42,17 @@ const App = () => {
   const itemslist = filteredItems.map(item => {
     return (
       <div className='item-wrapper'>
-        <div>itemid: {item.itemid}</div>
-        <div>name: {item.name}</div>
-        <div>tags: {item.tags}</div>
-        <div>price: {item.price}</div>
-        <div>description: {item.description}</div>
+        <div><b>id: </b>{item.itemid}</div>
+        <div><b>name: </b>{item.name}</div>
+        <div><b>tags: </b>{item.tags.map(tag => <span key={tag}>{tag}, </span>)}</div>
+        <div><b>price: </b>{item.price}</div>
+        <div><b>description: </b>{item.description}</div>
       </div>
     )
   });
 
-
-  const handleSearch = () => {
-    setSearchValue(searchValue);
-    if (searchValue.length >= 1) {
-
-
-      setActiveTags(activeTags.filter(tag => tag !== value));
-      } else {
-        setActiveTags([...activeTags, value]);
-      }
+  const handleToggleFilters = () => {
+    setShowFilters(!showFilters);
   }
 
   return (
@@ -72,24 +64,33 @@ const App = () => {
         <div className="logo-text">
           CampusXChange
         </div>
-        <div className="search-icon">
-          <FontAwesomeIcon icon={faSearch} size="lg" />
+        <div className="filter-icon" onClick={handleToggleFilters}>
+          <FontAwesomeIcon icon={faFilter} size="lg" />
         </div>
       </div>
       <div className="content">
-        <div className="filters-wrapper">
-          <div id="tag-filters" className="filter-group">
-            <TagFilter text="clothes" value="clothes" activeTags={activeTags} setActiveTags={setActiveTags} />
-            <TagFilter text="textbook" value="textbook" activeTags={activeTags} setActiveTags={setActiveTags} />
-          </div>
+        <div className="listings">
+          <text>Listings</text>
+        </div>
+        {showFilters && (
+          <div className="filters-wrapper">
+            <div id="tag-filters" className="filter-group">
+              <b>category</b>
+              <TagFilter text="clothes" value="clothes" activeTags={activeTags} setActiveTags={setActiveTags} />
+              <TagFilter text="textbook" value="textbook" activeTags={activeTags} setActiveTags={setActiveTags} />
+            </div>
 
-          <div id="price-filters" className="filter-group">
-            {filterprices.map(price => <PriceFilter text={`under ${price}`} activeFilter={activePriceFilter} setFilter={setActivePriceFilter} min={0} max={price} />)}
+            <div id="price-filters" className="filter-group">
+              <b>price</b>
+              {filterprices.map(price => <PriceFilter text={`under $${price}`} activeFilter={activePriceFilter} setFilter={setActivePriceFilter} min={0} max={price} />)}
+            </div>
+
+            <div className="search-bar">
+              <i>search name: </i>
+              <input name="" onChange={(event) => setSearchValue(event.target.value)}/>
+            </div>
           </div>
-        </div>
-        <div className="search-bar">
-          <input name="" onChange={(event) => setSearchValue(event.target.value)}/>
-        </div>
+        )}
         <div className="items-wrapper">
           {itemslist}
         </div>
@@ -99,7 +100,7 @@ const App = () => {
           <div className="footer-icons">
             <a href="/messages"><FontAwesomeIcon icon={faEnvelope} /></a>
             <a href="/marketplace"><FontAwesomeIcon icon={faStore} /></a>
-            <a href="/filter"><FontAwesomeIcon icon={faFilter} /></a>
+            <a href="/user"><FontAwesomeIcon icon={faUser} /></a>
           </div>
         </footer>
       </div>
