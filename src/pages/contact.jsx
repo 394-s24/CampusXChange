b5165eb4111dc044317bb3e7b2a6d15c2f57fec8
carolliu2024 <CarolLiu2024@u.northwestern.edu>
2024-04-06 18:37:12 -0500

    started email page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faSearch, faEnvelope, faStore, faFilter, faUser } from '@fortawesome/free-solid-svg-icons';
+import './contact.css'
+
+// import emailjs from '@emailjs/browser';
 
 /* Firebase */
 import { onValue } from "firebase/database";
@@ -10,6 +13,16 @@ export default function Contact({ textbookCountRef }) {
     return (
         <div className="contact-wrapper">
             <div>Hong Zhou</div>
+
+            {/* <p>Contact the seller here</p> */}
+
+            <form action="mailto:solaniosassyboi@gmail.com" method="post" className="contact-form"> 
+                <label>Email solaniosassyboi@gmail.com:</label>
+                <textarea name="message" placeholder='Type your message here'/>
+
+                <button type="submit">Send</button> 
+            </form>
+            
         </div>
     )
 }
